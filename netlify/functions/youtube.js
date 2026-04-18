6fb382dba3c2cea5ebce17304b22d7ad6ad559a1
pompomkisya <pompomkisya@gmail.com>
2026-04-19 03:33:56 +0900

    Update youtube.js

--- netlify/functions/youtube.js
+++ netlify/functions/youtube.js
@@ -10,7 +10,9 @@ exports.handler = async function(event) {
   }
 
   try {
-    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(query + ' pedal demo')}&type=video&maxResults=1&key=${apiKey}`;
+    // ダブルクォートで完全一致検索
+    const searchQuery = `"${query}"`;
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(searchQuery)}&type=video&maxResults=1&key=${apiKey}`;
     const res = await fetch(url);
     const data = await res.json();
     const videoId = data.items?.[0]?.id?.videoId || null;
